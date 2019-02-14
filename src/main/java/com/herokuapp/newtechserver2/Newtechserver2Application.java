6d10f49e66bb7d346b89a0ad46c48de31ff89cad
AbhishekKadam11 <abhishek.kadam007@gmail.com>
2019-02-14 22:28:48 +0530

    -Working on access resource image through url.

--- src/main/java/com/herokuapp/newtechserver2/Newtechserver2Application.java
+++ src/main/java/com/herokuapp/newtechserver2/Newtechserver2Application.java
@@ -2,13 +2,36 @@ package com.herokuapp.newtechserver2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
 public class Newtechserver2Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Newtechserver2Application.class, args);
+
+
+    }
+
+    @Configuration
+    @EnableWebMvc
+    public class MvcConfig implements WebMvcConfigurer {
+        @Override
+        public void addResourceHandlers(ResourceHandlerRegistry registry) {
+            registry
+                    .addResourceHandler("/resources/**")
+                    .addResourceLocations("/resources/","classpath:/other-resources/");
+        }
     }
 
 }
+
+
+
 
