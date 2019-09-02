a575427d42ac0329eb041d7e8f683c52ae5e700b
Abhishek Kadam <abhishek.kadam007@gmail.com>
2019-09-02 18:44:37 +0530

    -Fix heroku deployment issue.

--- src/main/java/com/herokuapp/newtechserver2/Newtechserver2Application.java
+++ src/main/java/com/herokuapp/newtechserver2/Newtechserver2Application.java
@@ -23,15 +23,7 @@ public class Newtechserver2Application {
         }
     }
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurerAdapter() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("*");
-            }
-        };
-    }
+
 }
 
 
